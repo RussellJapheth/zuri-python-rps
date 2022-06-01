828e9554d565cccf3eb37c828d08d2ce17dc617e
Russell Japheth <japheth.russell.i@gmail.com>
2022-06-01 23:13:14 +0100

    Created main.py and defined the algorithm for the game

---
+++ main.py
@@ -0,0 +1,17 @@
+# A simple cli Rock-Paper-Scissors game written in python
+
+# Steps
+# Display the rules and instructions for the game
+# Define the list of possible moves
+# Computer selects a move
+# Player selects a move (validate input; repeat until valid input)
+# Compare the moves and determine a winner
+# Display the results to the player
+# Ask if the player wants to continue or end the game
+# Repeat until player wants to end the game
+
+import random
+
+print('''
+    Welcome to Rock-Paper-Scissors!
+''')
